67ba83c356f6bc6aa01526f695f0b0206ab3fe6b
heyshizuna <shizuna.sugano@gmail.com>
2024-01-23 22:17:45 +0800

    footerRWD

--- JS/index-script.js
+++ JS/index-script.js
@@ -1,46 +1,61 @@
-var swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    direction: getDirection(),
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    991: {
+      //当屏幕宽度大于等于320
+      slidesPerView: 3,
+      spaceBetween: 20,
     },
-    on: {
-        resize: function () {
-            swiper.changeDirection(getDirection());
-        },
+    570: {
+      //当屏幕宽度大于等于320
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
+    320: {
+      //当屏幕宽度大于等于320
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  },
 });
+// GoTop //
+let goTop = document.querySelector(".goTop");
 
-function getDirection() {
-    var windowWidth = window.innerWidth;
-    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+goTopActive();
+window.addEventListener("scroll", goTopActive);
 
-    return direction;
-};
+function goTopActive() {
+  goTop.classList[window.scrollY >= 100 ? "add" : "remove"]("active");
+}
 // Lax.js 動畫
 window.onload = function () {
-    lax.init()
+  lax.init();
 
-    lax.addDriver('scrollY', function () {
-        return window.scrollY
-    });
+  lax.addDriver("scrollY", function () {
+    return window.scrollY;
+  });
 
-    lax.addElements('.about-imgL', {
-        scrollY: {
-            translateX: [
-              ['elInY','elCenterY','elOutY' ],
-              [0, '250', '500'],
-            ]
-          }
-          
-    })
-    
+  lax.addElements(".about-imgL", {
+    scrollY: {
+      translateX: [
+        ["elInY", "elCenterY", "elOutY"],
+        [0, "250", "500"],
+      ],
+    },
+  });
 };
 // AOS.js 動畫
 
-    AOS.init();
-
-    
+AOS.init();
